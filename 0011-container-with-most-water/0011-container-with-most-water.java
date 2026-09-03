67c22a51d8b3a244d58f1f753623ffa9a7f8f8c6
Ravi kushwaha <ravikushwaha13308@gmail.com>
2026-09-03 14:55:11 +0530

    Time: 5 ms (83.25%), Space: 77.2 MB (69.08%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,13 +1,24 @@
 class Solution {
     public int maxArea(int[] height) {
        int maxweight = 0;
+    
+       //Left Pointer 
        int left = 0;
+
+       //Right Pointer
        int right = height.length - 1;
+
        while(left < right){
         int weight = right - left;
+
+
         int ht = Math.min(height[left], height[right]);
         int current = weight * ht;
+
+        //According to question we are finding the Max weight
         maxweight = Math.max(maxweight , current);
+
+        
         if(height[left] < height[right]){
             left++;
         }else{
