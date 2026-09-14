fc9f9cfb0ed531de1cbd95ceb4fe3d97dc9ab8b8
Ravi kushwaha <ravikushwaha13308@gmail.com>
2026-09-14 22:38:47 +0530

    Time: 4 ms (36.2%), Space: 43.4 MB (40.76%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,28 +1,31 @@
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> ravi = new ArrayDeque<>();
+      Deque<Character> ravi =  new ArrayDeque<>();
+      
+    
 
-        for(int i = 0; i < s.length();i++){
-            char ch = s.charAt(i);
-           
+      for(int i = 0; i < s.length();i++){
+        char ch = s.charAt(i);
+        
+        if(ch == '('){
+            ravi.push(')');
+        }else if(ch == '{'){
+            ravi.push('}');
+        }else if(ch == '['){
+            ravi.push(']');
+        }else{
+            if(ravi.isEmpty()){
+                return false;
+            }
 
-            if(ch =='('){
-                ravi.push(')');
-            }else if(ch =='{'){
-                ravi.push('}');
-            }else if(ch =='['){
-                ravi.push(']');
-
-            }else{
-                if(ravi.isEmpty()){
-                    return false;
-                }
-                char expected = ravi.pop();
-                if(expected != ch){
-                    return false;
-                }
+            char expected = ravi.pop();
+            if(expected != ch){
+                return false;
             }
         }
-        return ravi.isEmpty();
+
+
+      }
+      return ravi.isEmpty();
     }
 }
