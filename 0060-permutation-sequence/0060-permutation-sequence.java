e318c870d73137fe70e94de105be26a2a869f13f
Ravi kushwaha <ravikushwaha13308@gmail.com>
2026-09-22 23:47:06 +0530

    Time: 2 ms (35.84%), Space: 42.9 MB (56.96%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> numbers = new ArrayList<>();
+        int[] factorial = new int[n];
+        factorial[0] = 1;
+        
+        for (int i = 1; i < n; i++) {
+            factorial[i] = factorial[i - 1] * i;
+            numbers.add(i);
+        }
+        numbers.add(n);
+        
+        k--; // Convert k to 0-based index
+        
+        StringBuilder result = new StringBuilder();
+        
+        for (int i = n; i >= 1; i--) {
+            int index = k / factorial[i - 1];
+            result.append(numbers.get(index));
+            numbers.remove(index);
+            k %= factorial[i - 1];
+        }
+        
+        return result.toString();
+    }
+}
